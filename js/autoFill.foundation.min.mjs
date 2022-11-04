a1f1cac9416cabda6b55f92da469e74738a2fd02
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 11:50:59 +0000

    d6b7ad76c39d470889acb35b9709af9865002f59
    Sync to source repo @d6b7ad76c39d470889acb35b9709af9865002f59

--- js/autoFill.foundation.min.mjs
+++ js/autoFill.foundation.min.mjs
@@ -1,1 +1,4 @@
+/*! Foundation integration for DataTables' AutoFill
+ * ©2015 SpryMedia Ltd - datatables.net/license
+ */
 import $ from"jquery";import DataTable from"datatables.net-zf";import AutoFill from"datatables.net-autofill";DataTable.AutoFill.classes.btn="button tiny";export default DataTable;
